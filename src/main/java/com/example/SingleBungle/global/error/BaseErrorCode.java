edb2371aa6398d3ce27d136fc1c3b2c9dd2d83f9
jiminnimij <124450012+jiminnimij@users.noreply.github.com>
2025-08-05 12:18:02 +0900

    feat: 전역 에러 처리

---
+++ src/main/java/com/example/SingleBungle/global/error/BaseErrorCode.java
@@ -0,0 +1,7 @@
+package com.example.SingleBungle.global.error;
+
+import com.example.SingleBungle.global.error.ErrorReason;
+
+public interface BaseErrorCode {
+    public ErrorReason getErrorReason();
+}
